d401c03cb55e0447a1416ed791cdbf7555ff76b1
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-08-25 15:02:32 -0300

    server.json change

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -11,7 +11,6 @@ export const Container = styled.div<IFoodPlateProps>`
   header {
     background: #ffb84d;
     border-radius: 8px 8px 0px 0px;
-    height: 192px;
     width: 405px;
     overflow: hidden;
     transition: 0.3s opacity;
